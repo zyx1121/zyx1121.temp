d448463aa31f70dbb65d671ba57be15aed4e4601
Loki <98001197+zyx1121@users.noreply.github.com>
2023-04-02 19:27:56 +0800

    updata

---
+++ rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc.d
@@ -0,0 +1,5 @@
+/Users/loki/Library/Mobile Documents/com~apple~CloudDocs/Temp/rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc: src/main.rs
+
+/Users/loki/Library/Mobile Documents/com~apple~CloudDocs/Temp/rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc.d: src/main.rs
+
+src/main.rs:
---
+++ rust-test/target/debug/rust-test.d
@@ -0,0 +1,1 @@
+/Users/loki/Library/Mobile\ Documents/com~apple~CloudDocs/Temp/rust-test/target/debug/rust-test: /Users/loki/Library/Mobile\ Documents/com~apple~CloudDocs/Temp/rust-test/src/main.rs
